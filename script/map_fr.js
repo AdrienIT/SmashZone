5844dfd7ecf9a250f06ccc4d9c823868518d70f8
arthur.vella@ynov.com <arthur.vella@ynov.com>
2020-05-25 13:04:59 +0200

    Dep selector DONE

--- script/map_fr.js
+++ script/map_fr.js
@@ -23,3 +23,51 @@ $(document).ready(function() {
         map.setSelectedRegions($(this).val());
     });
 });
+
+// Function to create the cookie 
+function createCookie(name, value) {
+    var expires;
+
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+
+    document.cookie = escape(name) + "=" +
+        escape(value) + expires + "; path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function getDepData() {
+    var selected_dep = document.getElementsByTagName("path")
+    var val = ""
+    for (var i = 0; i < selected_dep.length; i++) {
+        element = selected_dep[i].attributes
+        if (typeof(element) !== 'undefined' && element["fill"]["value"] == "yellow") {
+            val += element["data-code"]["value"].substring(3) + "-"
+        }
+
+    }
+    val = val.substring(0, val.length - 1)
+    dep_form = document.getElementById("dep")
+    dep_form.setAttribute('value', val)
+    document.getElementById("search").click();
+}
