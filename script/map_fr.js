41ff8f67f2d65b55b671d529b9af4779d4f4ac4e
arthur.vella@ynov.com <arthur.vella@ynov.com>
2020-05-26 13:03:34 +0200

    Filter done

--- script/map_fr.js
+++ script/map_fr.js
@@ -24,37 +24,6 @@ $(document).ready(function() {
     });
 });
 
-// Function to create the cookie 
-function createCookie(name, value) {
-    var expires;
-
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    } else {
-        expires = "";
-    }
-
-    document.cookie = escape(name) + "=" +
-        escape(value) + expires + "; path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 
 function getDepData() {
     var selected_dep = document.getElementsByTagName("path")
