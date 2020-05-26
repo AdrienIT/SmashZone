db9055cfbc03f7db46dd53289d594658e0c4eba5
Le Malgache <hyperpikaevo@gmail.com>
2020-05-26 16:40:24 +0200

    Added navbar collapse

--- index.php
+++ index.php
@@ -7,19 +7,30 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
             integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
         <link href="style/style.css" rel="stylesheet">
     </head>
 
     <body>
-        <nav class="navbar navbar-expand-sm navbar-dark flex-column flex-xl-row"
-            style="background-color: #264653; margin-bottom: 20px;">
-            <a href="index.php">
-                <div class="logo"><img class="main" src="style/SmashZone2.png" /><img class="ball"
-                        src="style/SmashZoneIcon.png" /></div>
+        <nav class="navbar navbar-expand-xl navbar-dark"
+            style="background-color: #264653; margin-bottom: 20px; height: 55px;">
+            <a class="logo" href="index.php">
+                <div><img class="main" src="style/SmashZone2.png" /><img class="ball" src="style/SmashZoneIcon.png" />
+                </div>
             </a>
-            </button>
-            <div class="collapse navbar-collapse d-inline-flex justify-content-left rubriques" id="navbarNav">
-                <ul class="navbar-nav">
+            <button class="navbar-toggler ml-auto" type=" button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span> </button>
+            <div class="collapse navbar-collapse rubriques" id="navbarNav">
+                <ul class=" navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-inline rubriquecolor">
                         Effectuer une recherche :
                     </li>
@@ -36,11 +47,12 @@
                             onclick="location.href='recherchejouer.php'" type="button">Tournois</button>
                     </form>
                 </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <button class="btn btn-outline-info my-2 my-sm-0" onclick="location.href='login_register/index.php'"
+                        type="button">Se
+                        connecter/S'inscrire</button>
+                </form>
             </div>
-            <form class="justify-content-between">
-                <button class="btn btn-outline-info my-2 my-sm-0" onclick="location.href='login.php'" type="button">Se
-                    connecter/S'inscrire</button>
-            </form>
         </nav>
         <div class="container">
             <h1>Dernières infos</h1>
