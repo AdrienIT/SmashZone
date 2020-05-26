3b9f86cc0c02fd14bdb1969823804044f2817e0e
AdrienIT <adrien.zoghbi@ynov.com>
2020-05-26 15:12:35 +0200

    commit

--- login_register/home.php
+++ login_register/home.php
@@ -1,1 +1,19 @@
-Profile du collegue
+<?php
+session_start();
+if (!isset($_SESSION["user_id"])) {
+	header('location: login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    <a href="logout.php">Se Déconnecter</a> <br>
+    <a href="update.php">Edition de profile</a>
+</body>
+</html>
