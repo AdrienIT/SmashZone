c2e22cf3dfe5c55b900d5ea02d21e21b4a78e15c
AdrienIT <adrien.zoghbi@ynov.com>
2020-05-27 17:26:06 +0200

    liste joueur 90%

---
+++ liste_joueurs.php
@@ -0,0 +1,46 @@
+<?php
+include_once 'config.php';
+
+$users = $db->query('SELECT * FROM users');
+
+$fetch = $users->fetch();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table style="border: 1px solid black;">
+        <tbody style="border: 1px solid black;" >
+        <tr style="border: 1px solid black;">
+            <td style="border: 1px solid black;">Pseudo</td>
+            <td style="border: 1px solid black;">Nom</td>
+            <td style="border: 1px solid black;">Prenom</td>
+            <td style="border: 1px solid black;">Classement</td>
+            <td style="border: 1px solid black;">telephone</td>
+            <td style="border: 1px solid black;">amis ?</td>
+        </tr>
+            <?php while ($u = $users->fetch()) { ?>
+                <tr style="border: 1px solid black;">
+                    <td style="border: 1px solid black;"><?= $u['pseudo'] ?></td>
+                    <td style="border: 1px solid black;"><?= $u['nom'] ?></td>
+                    <td style="border: 1px solid black;"><?= $u['prenom'] ?></td>
+                    <td style="border: 1px solid black;"><?= $u['classement'] ?></td>
+                    <td style="border: 1px solid black;"><?= $u['telephone'] ?></td>
+                    <td style="border: 1px solid black;"><a href="add_friend.php"></a>Add friend</td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
