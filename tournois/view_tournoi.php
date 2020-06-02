3bc1df3c74b71b25d4a190cab6925d3428c2c552
Le Malgache <hyperpikaevo@gmail.com>
2020-06-02 16:01:59 +0200

    basic CSS tournois

--- tournois/view_tournoi.php
+++ tournois/view_tournoi.php
@@ -11,14 +11,51 @@ $tournoi_infos = $get_infos->fetch()
 ?>
 <html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="../style/favicon.ico" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
+        <link href="../style/style.css" rel="stylesheet">
+        <title>Document</title>
+    </head>
 
-<body>
-    <?= var_dump($tournoi_infos) ?>
-</body>
+    <body>
+        <nav class="navbar navbar-expand-xl navbar-dark"
+            style="background-color: #264653; margin-bottom: 20px; height: 55px;">
+            <a class="logo" href="../index.php">
+                <div><img class="main" src="../style/SmashZone2.png" /><img class="ball"
+                        src="../style/SmashZoneIcon.png" />
+                </div>
+            </a>
+            <button class="navbar-toggler ml-auto" type=" button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span> </button>
+            </div>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-info my-2 my-sm-0" onclick="location.href='login_register/index.php'"
+                    type="button">Se
+                    connecter/S'inscrire</button>
+            </form>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-info my-2 my-sm-0" onclick="location.href='login_register/index.php'"
+                    type="button">Se
+                    connecter/S'inscrire</button>
+            </form>
+        </nav>
+
+        <?= var_dump($tournoi_infos) ?>
+    </body>
 
 </html>
