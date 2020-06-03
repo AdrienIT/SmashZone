89540c2baa9f7c570aa14bea40f206f0bc4cd405
Le Malgache <hyperpikaevo@gmail.com>
2020-06-03 13:07:27 +0200

    CSS admin en cours

--- admin/home.php
+++ admin/home.php
@@ -11,16 +11,71 @@ if (!isset($_SESSION["admin_id"])) {
 
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="../style/favicon.ico" />
-        <title>Homepage</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="../style/style.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Administration</title>
     </head>
 
     <body>
-        <h1>Bienvenue Admin</h1>
-        <a href="users.php">Utilisateurs</a> <br> <br>
-        <a href="clubs.php">clubs</a> <br> <br>
-        <a href="logout.php">Logout</a>
+        <nav class="navbar navbar-expand-xl navbar-dark"
+            style="background-color: #264653; margin-bottom: 20px; height: 55px;">
+            <a class="logo" href="../index.php">
+                <div><img class="main" src="../style/SmashZone2.png" /><img class="ball"
+                        src="../style/SmashZoneIcon.png" />
+                </div>
+            </a>
+            <button class="navbar-toggler ml-auto" type=" button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span> </button>
+            <div class="collapse navbar-collapse rubriques" id="navbarNav">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item rubriquecolor">
+                        Recherchez :
+                    </li>
+                    <form class="form-inline">
+                        <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch"
+                            onclick="location.href='../offres/list_offers.php'" type="button">Partenaires</button>
+                    </form>
+                    <form class="form-inline">
+                        <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch"
+                            onclick="location.href='../liste_joueurs.php'" type="button">Joueurs</button>
+                    </form>
+                    <form class="form-inline">
+                        <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch"
+                            onclick="location.href='recherchejouer.php'" type="button">Tournois</button>
+                    </form>
+                    <form class="form-inline">
+                        <button class="btn btn-outline-light my-2 my-sm-0 rubriquesearch"
+                            onclick="location.href='../offres/new_offer.php'" type="button">Poster une annonce</button>
+                    </form>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-auto">
+                    <h1>Bienvenue Admin</h1>
+                    <a class="btn btn-light btn-block" href="users.php">Gestion utilisateurs</a> <br> <br>
+                    <a class="btn btn-success btn-block" href="clubs.php">Gestion clubs</a> <br> <br>
+                    <a class="btn btn-danger btn-block" href="logout.php">Se déconnecter</a>
+                </div>
+            </div>
+        </div>
     </body>
 
 </html>
