69e13e5b3c5d9764a0fa5b6270c170d7837bf0ab
Le Malgache <hyperpikaevo@gmail.com>
2020-06-16 23:08:55 +0200

    bootstrap total clubs

--- club/choix_gagnant.php
+++ club/choix_gagnant.php
@@ -19,19 +19,75 @@ $u = $query2->fetchAll();
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
+    <title>Tournois du <?php echo $user['nom_club'] ?></title>
+
+    <!-- Important ! -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Choix gagant</title>
+    <link rel="icon" href="../style/favicon.ico" />
+    <meta charset="utf-8">
+    <!-- -->
+
+    <!-- Scripts au chargement de la page -->
+    <script src="../script/checkbox.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+    <link href="../style/style.css" rel="stylesheet">
+    <link href="../style/home.css" rel="stylesheet">
+    <link href="../style/notification.css" rel="stylesheet">
+    <script>
+        var notifs = <?php echo json_encode($all_notifs) ?>
+    </script>
+    <!-- Scripts au chargement de la page -->
+
 </head>
-<body>
-    <table>
-        <tr>
+
+<body onload="loadNotifi(notifs)">
+
+    <!-- Barre de navigation -->
+    <nav class="navbar navbar-expand-xl navbar-dark mb-4" style="background-color: #264653; height: 55px;">
+        <a class="navbar-brand main" href="../index.php">
+            <img class="main" src="../style/SmashZone2.png" /><img class="ball" src="../style/SmashZoneIcon.png" />
+        </a>
+
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03" style="background-color: #264653;">
+
+            <div class="icon" onclick="toggleNotifi()" id="notif"></div>
+            <div class="notifi-box" id="box">
+            </div>
+
+        </div>
+    </nav>
+    <!-- Fin barre de navigation -->
+<h1 class="mb-4 font-weight-bold">Liste des tournois</h1>
+<table class="table">
+    <thead class="thead-dark text-center">
+        <tr class="joueurborder">
             <td>Pseudo</td>
             <td>Nom</td>
-            <td>Prenom</td>
-            <td>Choisir ?</td>
+            <td>Prénom</td>
+            <td>Sélection</td>
         </tr>
+    <tr>
         <?php foreach($u as $users) { ?>
         <tr>
             <td> <?= $users['pseudo'] ?> </td>
@@ -41,5 +97,6 @@ $u = $query2->fetchAll();
         </tr>
         <?php } ?>
     </table>
+        </tbody>
 </body>
 </html>
