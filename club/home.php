550c9ad96b41cc978071c5cf0bc92704b56bcfb5
arthur.vella@ynov.com <arthur.vella@ynov.com>
2020-06-16 16:16:49 +0200

    Lots of bug fixed

--- club/home.php
+++ club/home.php
@@ -13,78 +13,73 @@ $query->execute();
 
 $user = $query->fetch();
 
+if (file_exists(str_replace(" ", "_", $user['nom_club']))) {
+    $img_src = str_replace(" ", "_", $user['nom_club']) . "/" . str_replace(" ", "_", $user['nom_club']) . ".png";
+} else {
+    $img_src = "default-club.png";
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 
-    <head>
-        <link rel="icon" href="../style/favicon.ico" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-        </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-        </script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-        </script>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link href="../style/style.css" rel="stylesheet">
-        <title>Homepage</title>
-    </head>
+<head>
+    <link rel="icon" href="../style/favicon.ico" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="../style/style.css" rel="stylesheet">
+    <title>Homepage</title>
+</head>
 
-    <body>
-        <nav class="navbar navbar-expand-xl navbar-dark"
-            style="background-color: #264653; margin-bottom: 20px; height: 55px;">
-            <a class="logo" href="../index.php">
-                <div><img class="main" src="../style/SmashZone2.png" /><img class="ball"
-                        src="../style/SmashZoneIcon.png" />
-                </div>
-            </a>
-            <button class="navbar-toggler ml-auto" type=" button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span> </button>
-            <div class="collapse navbar-collapse rubriques" id="navbarNav">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item rubriquecolor">
-                        Recherchez :
-                    </li>
-                    <form class="form-inline">
-                        <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch"
-                            onclick="location.href='../offres/list_offers.php'" type="button">Partenaires</button>
-                    </form>
-                    <form class="form-inline">
-                        <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch"
-                            onclick="location.href='../liste_joueurs.php'" type="button">Joueurs</button>
-                    </form>
-                    <form class="form-inline">
-                        <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch"
-                            onclick="location.href='recherchejouer.php'" type="button">Tournois</button>
-                    </form>
-                    <form class="form-inline">
-                        <button class="btn btn-outline-light my-2 my-sm-0 rubriquesearch"
-                            onclick="location.href='../offres/new_offer.php'" type="button">Poster une annonce</button>
-                    </form>
-                </ul>
+<body>
+    <nav class="navbar navbar-expand-xl navbar-dark" style="background-color: #264653; margin-bottom: 20px; height: 55px;">
+        <a class="logo" href="../index.php">
+            <div><img class="main" src="../style/SmashZone2.png" /><img class="ball" src="../style/SmashZoneIcon.png" />
             </div>
-        </nav>
+        </a>
+        <button class="navbar-toggler ml-auto" type=" button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span> </button>
+        <div class="collapse navbar-collapse rubriques" id="navbarNav">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item rubriquecolor">
+                    Recherchez :
+                </li>
+                <form class="form-inline">
+                    <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch" onclick="location.href='../offres/list_offers.php'" type="button">Partenaires</button>
+                </form>
+                <form class="form-inline">
+                    <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch" onclick="location.href='../liste_joueurs.php'" type="button">Joueurs</button>
+                </form>
+                <form class="form-inline">
+                    <button class="btn btn-outline-warning my-2 my-sm-0 rubriquesearch" onclick="location.href='recherchejouer.php'" type="button">Tournois</button>
+                </form>
+                <form class="form-inline">
+                    <button class="btn btn-outline-light my-2 my-sm-0 rubriquesearch" onclick="location.href='../offres/new_offer.php'" type="button">Poster une annonce</button>
+                </form>
+            </ul>
+        </div>
+    </nav>
 
-        <div class="container text-center">
-            <div class="row justify-content-center">
-                <div class="col-auto">
-                    <img src=<?php echo $user['nom_club'] . "/" . $user['nom_club'] . ".png" ?>
-                    <h1> Bonjour <?php echo $user['nom_club'] ?></h1>
-                    <a class="btn btn-light btn-block" href="update.php">Editer le club</a> <br> <br>
-                    <a class="btn btn-light btn-block" href="mes_tournois.php">Mes Tournois</a> <br> <br>
-                    <a class="btn btn-light btn-block" href="avatar.php">Photo du club</a> <br> <br>
-                    <a class="btn btn-danger btn-block" href="logout.php">Se Déconnecter</a>
-                </div>
+    <div class="container text-center">
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <img src=<?= $img_src ?>>
+                <h1> Bonjour <?php echo $user['nom_club'] ?></h1>
+                <a class="btn btn-light btn-block" href="update.php">Editer le club</a> <br> <br>
+                <a class="btn btn-light btn-block" href="mes_tournois.php">Mes Tournois</a> <br> <br>
+                <a class="btn btn-light btn-block" href="avatar.php">Photo du club</a> <br> <br>
+                <a class="btn btn-danger btn-block" href="logout.php">Se Déconnecter</a>
             </div>
         </div>
-    </body>
+    </div>
+</body>
 
 </html>
--- liste_clubs.php
+++ liste_clubs.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION["user_id"])) {
     $id = (int) $_SESSION["user_id"];
 }
 
-$query = $db->prepare('SELECT nom_club,telephone FROM clubs WHERE club_id = :club_id');
+$query = $db->prepare('SELECT * FROM clubs WHERE club_id = :club_id');
 $query->bindParam(':club_id', $id);
 $query->execute();
 
@@ -40,17 +40,13 @@ if (isset($_POST['recherche'])) {
     <!-- -->
 
     <!-- Scripts au chargement de la page -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
@@ -72,44 +68,36 @@ if (isset($_POST['recherche'])) {
             <img class="main" src="style/SmashZone2.png" /><img class="ball" src="style/SmashZoneIcon.png" />
         </a>
 
-        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
-            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03" style="background-color: #264653;">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0 float-right text-right">
                 <li class="nav-item mr-2">
-                    <button class="btn btn-outline-warning" onclick="location.href='offres/list_offers.php'"
-                        type="button">Partenaires</button>
+                    <button class="btn btn-outline-warning" onclick="location.href='offres/list_offers.php'" type="button">Partenaires</button>
                 </li>
                 <li class="nav-item mr-2">
-                    <button class="btn btn-outline-warning" onclick="location.href='liste_joueurs.php'"
-                        type="button">Joueurs</button>
+                    <button class="btn btn-outline-warning" onclick="location.href='liste_joueurs.php'" type="button">Joueurs</button>
                 </li>
                 <li class="nav-item mr-2">
-                    <button class="btn btn-outline-warning" onclick="location.href='tournois/liste_tournoi.php'"
-                        type="button">Tournois</button>
+                    <button class="btn btn-outline-warning" onclick="location.href='tournois/liste_tournoi.php'" type="button">Tournois</button>
                 </li>
                 <li class="nav-item mr-2">
-                    <button class="btn btn-outline-warning" onclick="location.href='classement.php'"
-                        type="button">Classement</button>
+                    <button class="btn btn-outline-warning" onclick="location.href='classement.php'" type="button">Classement</button>
                 </li>
                 <li class="nav-item mr-2">
-                    <button class="btn btn-outline-warning" onclick="location.href='liste_clubs.php'"
-                        type="button">Clubs</button>
+                    <button class="btn btn-outline-warning" onclick="location.href='liste_clubs.php'" type="button">Clubs</button>
                 </li>
                 <li class="nav-item">
-                    <button class="btn btn-outline-light" onclick="location.href='offres/new_offer.php'"
-                        type="button">Poster une annonce</button>
+                    <button class="btn btn-outline-light" onclick="location.href='offres/new_offer.php'" type="button">Poster une annonce</button>
                 </li>
             </ul>
 
             <div class="icon" onclick="toggleNotifi()" id="notif"></div>
             <div class="notifi-box" id="box">
             </div>
-            <button class="btn btn-outline-info my-2 my-sm-0" onclick="location.href='login_register/login.php'"
-                type="button"><?= $connect ?></button>
+            <button class="btn btn-outline-info my-2 my-sm-0" onclick="location.href='login_register/login.php'" type="button"><?= $connect ?></button>
         </div>
     </nav>
     <!-- Fin barre de navigation -->
@@ -125,7 +113,7 @@ if (isset($_POST['recherche'])) {
             <thead class="thead-dark text-center">
                 <tr class="joueurborder">
                     <th>Nom du club</td>
-                    <th>Télephone</td>
+                    <th>Ville</td>
                     <th>Profil</td>
                 </tr>
             <tbody>
@@ -133,7 +121,7 @@ if (isset($_POST['recherche'])) {
                     while ($qs = $querysearch->fetch()) { ?>
                         <tr>
                             <td><?= $qs['nom_club'] ?></td>
-                            <td><?= $qs['telephone'] ?></td>
+                            <td><?= $qs['ville'] ?></td>
                             <td class="text-center">
                                 <a type='submit' name='contact' href='infos_clubs.php?contact=<?php echo $qs['club_id'] ?>' class=' btn btn-primary'>Voir le profil</a> </td> <?php } ?>
 
@@ -142,7 +130,7 @@ if (isset($_POST['recherche'])) {
                         while ($u = $users->fetch()) { ?>
                             <tr>
                                 <td><?= $u['nom_club'] ?></td>
-                                <td><?= $u['telephone'] ?></td>
+                                <td><?= $u['ville'] ?></td>
                                 <td class="text-center">
                                     <a type='submit' name='contact' href='infos_clubs.php?contact=<?php echo $u['club_id'] ?>' class=' btn btn-primary'>Voir le profil</a> </td>
                         <?php }
